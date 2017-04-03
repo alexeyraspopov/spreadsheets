544a42560a584a7fa3ff01c1b361b903a61f15f3
Alexey Raspopov <alexey.raspopov@datarobot.com>
2017-04-03 10:04:31 +0300

    update projects data

--- index.js
+++ index.js
@@ -3,26 +3,16 @@ import ReactDOM from 'react-dom';
 import App from './modules/App';
 
 const projects = [
-  { id: 'P0', projectName: 'Prediction Application Test Project',
-    filename: '10k-diabetes-train.csv', createdAt: new Date(), role: 'Owner' },
-  { id: 'P1', projectName: 'Prediction Application Test Project',
-    filename: '10k-diabetes-train.csv', createdAt: new Date(), role: 'Owner' },
-  { id: 'P2', projectName: 'Prediction Application Test Project',
-    filename: '10k-diabetes-train.csv', createdAt: new Date(), role: 'Owner' },
-  { id: 'P3', projectName: 'Prediction Application Test Project',
-    filename: '10k-diabetes-train.csv', createdAt: new Date(), role: 'Owner' },
-  { id: 'P4', projectName: 'Prediction Application Test Project',
-    filename: '10k-diabetes-train.csv', createdAt: new Date(), role: 'Owner' },
-  { id: 'P5', projectName: 'Prediction Application Test Project',
-    filename: '10k-diabetes-train.csv', createdAt: new Date(), role: 'Owner' },
-  { id: 'P6', projectName: 'Prediction Application Test Project',
-    filename: '10k-diabetes-train.csv', createdAt: new Date(), role: 'Owner' },
-  { id: 'P7', projectName: 'Prediction Application Test Project',
-    filename: '10k-diabetes-train.csv', createdAt: new Date(), role: 'Owner' },
-  { id: 'P8', projectName: 'Prediction Application Test Project',
-    filename: '10k-diabetes-train.csv', createdAt: new Date(), role: 'Owner' },
-  { id: 'P9', projectName: 'Prediction Application Test Project',
-    filename: '10k-diabetes-train.csv', createdAt: new Date(), role: 'Owner' },
+  { id: 'P0', projectName: 'The best project ever',
+    filename: '10k-diabetes-train.csv', createdAt: new Date('2017-04-01'), role: 'Admin' },
+  { id: 'P1', projectName: 'Just testing some stuff',
+    filename: 'fastiron-sample.csv', createdAt: new Date('2017-01-01'), role: 'Owner' },
+  { id: 'P2', projectName: 'Mama, just killed a man',
+    filename: 'listofpeopleiwannamurder.csv', createdAt: new Date('2016-08-01'), role: 'Admin' },
+  { id: 'P3', projectName: 'My hands are typing',
+    filename: 'hdfs://storage.nsa.gov/citizens-browsing-history', createdAt: new Date('2017-04-01'), role: 'Observer' },
+  { id: 'P4', projectName: 'This is why I\'m hot',
+    filename: 'tinder-big-data.csv', createdAt: new Date('2012-04-01'), role: 'Owner' },
 ];
 
 const hotspots = [
